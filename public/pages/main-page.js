10e26b17d56befbfed6150481c603a2d6d3e9a63
GabrielaRayla <grgabrielarayla@gmail.com>
2023-02-26 20:55:30 -0300

    responsive header by filipe

--- public/pages/main-page.js
+++ public/pages/main-page.js
@@ -9,18 +9,20 @@ export function Categories() {
 
     div.innerHTML = `
 	<header class="flex-row-between">
-	<div class="header-items flex-row-between">
-		<img id="logo" src="../assets/icons/logo-godpedia.png" alt="">
-		<div class="container-search flex-row-center">
-		   <input class="search-input" type="text"> 
-		   <img class="search-icon" src="../assets/icons/search.svg" alt="">
-		</div>
-	</div>
-	<div class="header-items flex-row-between">
-		<img id="profile-icon" src="../assets/icons/profile.svg" alt="">
-		<img id="menu-icon" src="../assets/icons/menu.svg" alt="">
-	</div>
-</header>
+        <div class="header-items flex-row-center logo-search">
+            <img id="logo" src="../assets/icons/logo-godpedia.png" alt="">
+            <div class="container-search flex-row-center">
+            <input class="search-input" type="text"> 
+            <div class="search">
+                    <img class="search-icon" src="../assets/icons/search.svg" alt="">
+            </div>
+            </div>
+        </div>
+        <div class="header-items flex-row-end menu-icon-div">
+            <img id="profile-icon" src="../assets/icons/profile.svg" alt="">
+            <img id="menu-icon" src="../assets/icons/menu.svg" alt="">
+        </div>
+    </header>
 <main class="main">
 	<div class="container-carousel-category">
 		<div class="flex-row-between">
--- public/pages/principal-teste.js
+++ public/pages/principal-teste.js
@@ -7,18 +7,21 @@ export function PrincipalTeste() {
 
     div.innerHTML = `
 	<header class="flex-row-between">
-		<div class="header-items flex-row-between">
-			<img id="logo" src="../assets/icons/logo-godpedia.png" alt="">
-			<div class="container-search flex-row-center">
-			<input class="search-input" type="text"> 
-			<img class="search-icon" src="../assets/icons/search.svg" alt="">
-			</div>
-		</div>
-		<div class="header-items flex-row-between">
-			<img id="profile-icon" src="../assets/icons/profile.svg" alt="">
-			<img id="menu-icon" src="../assets/icons/menu.svg" alt="">
-		</div>
-	</header>
+        <div class="header-items flex-row-center logo-search">
+            <img id="logo" src="../assets/icons/logo-godpedia.png" alt="">
+            <div class="container-search flex-row-center">
+            <input class="search-input" type="text"> 
+            <div class="search">
+                    <img class="search-icon" src="../assets/icons/search.svg" alt="">
+            </div>
+            </div>
+        </div>
+        <div class="header-items flex-row-end menu-icon-div">
+            <img id="profile-icon" src="../assets/icons/profile.svg" alt="">
+            <img id="menu-icon" src="../assets/icons/menu.svg" alt="">
+        </div>
+    </header>
+	
 	<div id="background-space">
 		<main class="main-landing-page flex-center-center">
 			<div class="container-total flex-center-center">
